f351d1f65657797ff2e9833d5a6881a8b16db0fa
Joanna <zacharowasia@wp.pl>
2024-06-23 22:05:53 +0200

    zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .json

--- main.py
+++ main.py
@@ -23,3 +23,23 @@ def json_file(file_path):
         print("An error occurred: ", e)
 
     return None
+
+def save_json(data, file_path):
+    try:
+        if not os.path.exists(file_path):
+            directory = os.path.dirname(file_path)
+            if directory:
+                os.makedirs(directory, exist_ok=True)
+            else:
+                file_path = os.path.join(os.getcwd(), file_path)
+                directory = os.path.dirname(file_path)
+                os.makedirs(directory, exist_ok=True)
+            with open(file_path, 'w') as file:
+                file.write('{}\n')
+            print("Created new JSON file: ", file_path)
+
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+        print("Data saved successfully.")
+    except Exception as e:
+        print("Error: ", e)
